57b2a22468e7083a194aa4c1fe4e8fb9b147689c
Jimmy McDermott <mcdappdev@gmail.com>
2018-12-02 16:43:07 -0500

    decoder extensions

--- Sources/Jobs/Job.swift
+++ Sources/Jobs/Job.swift
@@ -1,20 +1,24 @@
 import Vapor
 
 public protocol Job: Codable {
-    static func register<T: Job>(_ jobType: T.Type)
-    func dispatch(context: JobContext) throws -> EventLoopFuture<Void>
+    static func register()
+    func dispatch(context: JobContext, worker: EventLoopGroup) throws -> EventLoopFuture<Void>
 }
 
 fileprivate typealias JobTypeDecoder = (Decoder) throws -> Job
 fileprivate var knownJobTypes: [String: JobTypeDecoder] = [:]
 
-extension Job {
-    static func register<T: Job>(_ jobType: T.Type = T.self) {
-        knownJobTypes[String(describing: T.self)] = T.init(from: )
+extension Job where Self: Job {
+    static func register() {
+        knownJobTypes[String(describing: Self.self)] = Self.init(from: )
     }
     
-    static func decode(jobType: String, from decoder: Decoder) throws -> Job {
-        guard let jobDecoder = knownJobTypes[jobType] else { throw DecodingError.dataCorrupted(.init(codingPath: decoder.codingPath, debugDescription: "Unknown job type \(jobType)")) }
+    static func decode(from decoder: Decoder) throws -> Job {
+        let jobType = String(describing: Self.self)
+        guard let jobDecoder = knownJobTypes[jobType] else {
+            throw DecodingError.dataCorrupted(.init(codingPath: decoder.codingPath, debugDescription: "Unknown job type \(jobType)"))
+        }
+        
         return try jobDecoder(decoder)
     }
 }
