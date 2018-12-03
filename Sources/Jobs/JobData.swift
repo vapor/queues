252c8a7e02fcb3f2db47bc9dd87879bd0c94a748
Jimmy McDermott <mcdappdev@gmail.com>
2018-12-03 14:40:37 -0500

    jobs

--- Sources/Jobs/JobData.swift
+++ Sources/Jobs/JobData.swift
@@ -29,7 +29,7 @@ struct JobData: Codable {
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(self.key, forKey: .key)
-        let typeString = String(describing: self.data)
+        let typeString = String(describing: type(of: self.data))
         try container.encode(typeString, forKey: .type)
         
         let superEncoder = container.superEncoder(forKey: .data)
