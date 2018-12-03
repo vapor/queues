7bdb03761b369e71663cff8119438eca130c2f7a
Jimmy McDermott <mcdappdev@gmail.com>
2018-12-03 14:20:26 -0500

    make methods public

--- Sources/Jobs/Job.swift
+++ Sources/Jobs/Job.swift
@@ -12,14 +12,14 @@ internal typealias JobTypeDecoder = (Decoder) throws -> Job
 internal var knownJobTypes: [String: JobTypeDecoder] = [:]
 
 extension Job {    
-    func error(context: JobContext, error: Error, worker: EventLoopGroup) -> EventLoopFuture<Void> {
+    public func error(context: JobContext, error: Error, worker: EventLoopGroup) -> EventLoopFuture<Void> {
         return worker.future()
     }
 }
 
 //TODO: - Clean these methods up and add a helper config type
 extension Job where Self: Job {
-    static func register() {
+    public static func register() {
         knownJobTypes[String(describing: Self.self)] = Self.init(from: )
     }
 }
