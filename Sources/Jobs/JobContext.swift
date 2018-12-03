6b3b7eeffc7734f48334c99432746ec884bd0948
Jimmy McDermott <mcdappdev@gmail.com>
2018-12-02 22:07:05 -0500

    add jobcontext example

--- Sources/Jobs/JobContext.swift
+++ Sources/Jobs/JobContext.swift
@@ -1,6 +1,23 @@
 import Foundation
 import Vapor
 
-public struct JobContext: Service, Codable {
-    public init() { }
+public struct JobContext: Service {
+    public var userInfo: [String: Any]
+    
+    public init() {
+        self.userInfo = [:]
+    }
+}
+
+struct TestingService: Service { }
+
+extension JobContext {
+    var testingService: TestingService? {
+        get {
+            return userInfo[String(describing: self)] as? TestingService
+        }
+        set {
+            userInfo[String(describing: self)] = newValue
+        }
+    }
 }
