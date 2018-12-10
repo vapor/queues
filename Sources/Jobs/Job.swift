82009833a7b4dac9de32473fbbf9a06386987e05
Jimmy McDermott <mcdappdev@gmail.com>
2018-12-10 17:15:35 -0500

    remove static register method

--- Sources/Jobs/Job.swift
+++ Sources/Jobs/Job.swift
@@ -2,8 +2,6 @@ import Vapor
 
 public protocol Job: Codable {
     var maxRetryCount: Int { get }
-    
-    static func register()
     func dequeue(context: JobContext, worker: EventLoopGroup) throws -> EventLoopFuture<Void>
     func error(context: JobContext, error: Error, worker: EventLoopGroup) -> EventLoopFuture<Void>
 }
