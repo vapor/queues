c412c8931723b4f950148a149f2f4bc2e8062917
Jimmy McDermott <mcdappdev@gmail.com>
2018-12-11 15:18:03 -0500

    safer sigterm trapping

--- Sources/Jobs/JobsCommand.swift
+++ Sources/Jobs/JobsCommand.swift
@@ -30,7 +30,6 @@ public struct JobsCommand: Command {
         let console = context.console
         let queue = QueueType(name: context.options["queue"] ?? QueueType.default.name)
         let key = queue.makeKey(with: queueService.persistenceKey)
-        var shuttingDownHandler = JobsShuttingDownHandler()
         
         let quiesce = ServerQuiescingHelper(group: elg)
         let signalQueue = DispatchQueue(label: "vapor.jobs.command.SignalHandlingQueue")
@@ -39,46 +38,40 @@ public struct JobsCommand: Command {
         signalSource.setEventHandler {
             print("SIGTERM RECEIVED")
             signalSource.cancel()
-            shuttingDownHandler.set(isShuttingDown: true)
             quiesce.initiateShutdown(promise: fullyShutdownPromise)
         }
         signal(SIGTERM, SIG_IGN)
         signalSource.resume()
         
-        for i in 0..<System.coreCount {
-            print("Scheduling work on #\(i)")
-            let eventLoop = elg.next()
-            
-            _ = eventLoop.scheduleRepeatedTask(initialDelay: .seconds(0), delay: queueService.refreshInterval) { task -> EventLoopFuture<Void> in
-                if !shuttingDownHandler.get() {
-                    return queueService.persistenceLayer.get(key: key).flatMap { jobData in
-                        //No job found, go to the next iteration
-                        guard let jobData = jobData else { return eventLoop.future() }
-                        let job = jobData.data
-                        console.info("Dequeing Job", newLine: true)
-                        
-                        let futureJob = job.dequeue(context: jobContext, worker: elg)
-                        return self.firstFutureToSucceed(future: futureJob, tries: jobData.maxRetryCount, on: elg)
-                            .flatMap { _ in
-                                guard let jobString = job.stringValue(key: key, maxRetryCount: jobData.maxRetryCount) else {
-                                    return eventLoop.future(error: Abort(.internalServerError))
-                                }
-                                
-                                return queueService.persistenceLayer.completed(key: key, jobString: jobString)
+        var repeatedTasks: [RepeatedTask] = []
+        for eventLoop in elg.makeIterator()! {
+            let task = eventLoop.scheduleRepeatedTask(initialDelay: .seconds(0), delay: queueService.refreshInterval) { task -> EventLoopFuture<Void> in
+                return queueService.persistenceLayer.get(key: key).flatMap { jobData in
+                    //No job found, go to the next iteration
+                    guard let jobData = jobData else { return eventLoop.future() }
+                    let job = jobData.data
+                    console.info("Dequeing Job", newLine: true)
+                    
+                    let futureJob = job.dequeue(context: jobContext, worker: elg)
+                    return self.firstFutureToSucceed(future: futureJob, tries: jobData.maxRetryCount, on: elg)
+                        .flatMap { _ in
+                            guard let jobString = job.stringValue(key: key, maxRetryCount: jobData.maxRetryCount) else {
+                                return eventLoop.future(error: Abort(.internalServerError))
                             }
-                            .catchFlatMap { error in
-                                console.error("Job error: \(error)", newLine: true)
-                                return job.error(context: jobContext, error: error, worker: elg).transform(to: ())
+                            
+                            return queueService.persistenceLayer.completed(key: key, jobString: jobString)
                         }
+                        .catchFlatMap { error in
+                            console.error("Job error: \(error)", newLine: true)
+                            return job.error(context: jobContext, error: error, worker: elg).transform(to: ())
                     }
                 }
-                
-                return eventLoop.future()
             }
             
+            repeatedTasks.append(task)
         }
-
-        return fullyShutdownPromise.futureResult
+        
+        return fullyShutdownPromise.futureResult.map { repeatedTasks.forEach { $0.cancel() } }
     }
     
     /// Returns the first time a given future succeeds and is under the `tries`
@@ -98,42 +91,5 @@ public struct JobsCommand: Command {
                 return self.firstFutureToSucceed(future: future, tries: tries - 1, on: worker)
             }
         }
-    }
-}
-
-
-/// A handler to synchronize the access of `isShuttingDown`
-private struct JobsShuttingDownHandler {
-    
-    /// The Lock used for synchronizing
-    private let lock: NSLock
-    
-    /// Whether or not the app is shutting down
-    private var isShuttingDown: Bool
-    
-    /// Creates a new handler
-    init() {
-        self.lock = .init()
-        self.isShuttingDown = false
-    }
-    
-    /// Sets the shutting down variable
-    ///
-    /// - Parameter isShuttingDown: Whether or not the app is shutting down
-    mutating func set(isShuttingDown: Bool) {
-        lock.lock()
-        defer { lock.unlock() }
-        
-        self.isShuttingDown = isShuttingDown
-    }
-    
-    /// Returns the `isShuttingDown` value
-    ///
-    /// - Returns: Whether or not the app is shutting fown
-    func get() -> Bool {
-        lock.lock()
-        defer { lock.unlock() }
-        
-        return isShuttingDown
     }
 }
