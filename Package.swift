6b918329171696de2bac4fce66f05227771fc9ec
Tanner <me@tanner.xyz>
2019-02-12 13:37:12 -0500

    add circle ci file (#3)
    * add circle ci file
    
    * add redis driver to tests
    
    * fix circle typo
    
    * fix package swift
    
    * fix tests
    
    * use swift 4.2 image

--- Package.swift
+++ Package.swift
@@ -1,23 +1,16 @@
 // swift-tools-version:4.2
-
 import PackageDescription
 
 let package = Package(
-    name: "Jobs",
+    name: "jobs",
     products: [
-        .library(
-            name: "Jobs",
-            targets: ["Jobs"]),
+        .library(name: "Jobs", targets: ["Jobs"]),
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
     ],
     targets: [
-        .target(
-            name: "Jobs",
-            dependencies: ["Vapor"]),
-        .testTarget(
-            name: "JobsTests",
-            dependencies: ["Jobs"]),
+        .target(name: "Jobs", dependencies: ["Vapor"]),
+        .testTarget(name: "JobsTests", dependencies: ["Jobs"]),
     ]
 )
