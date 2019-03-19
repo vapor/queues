31c04669894dfd0786dcdf90271667dfa8c9ff76
Raul Riera <rieraraul@gmail.com>
2019-03-18 20:32:54 -0400

    Support for testing JobStorage, Queuename, and JobConfig (#10)

--- Tests/JobsTests/XCTestManifests.swift
+++ Tests/JobsTests/XCTestManifests.swift
@@ -1,9 +1,37 @@
 import XCTest
 
+extension JobStorageTests {
+    static let __allTests = [
+        ("testStringRepresentationIsValidJSON", testStringRepresentationIsValidJSON),
+    ]
+}
+
+extension JobsConfigTests {
+    static let __allTests = [
+        ("testAddingAlreadyRegistratedJobsAreIgnored", testAddingAlreadyRegistratedJobsAreIgnored),
+        ("testAddingJobs", testAddingJobs),
+    ]
+}
+
+extension JobsTests {
+    static let __allTests = [
+        ("testStub", testStub),
+    ]
+}
+
+extension QueueNameTests {
+    static let __allTests = [
+        ("testKeyIsGeneratedCorrectly", testKeyIsGeneratedCorrectly),
+    ]
+}
+
 #if !os(macOS)
-public func allTests() -> [XCTestCaseEntry] {
+public func __allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(JobsTests.allTests),
+        testCase(JobStorageTests.__allTests),
+        testCase(JobsConfigTests.__allTests),
+        testCase(JobsTests.__allTests),
+        testCase(QueueNameTests.__allTests),
     ]
 }
 #endif
