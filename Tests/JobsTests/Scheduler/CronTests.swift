c2a884860b15cd5fcafc5dedcd4c247c569bd8f7
{rnantes} <{reid.nantes@gmail.com}>
2019-05-29 20:39:10 -0400

    Changed to proper Scheduling method

--- Tests/JobsTests/Scheduler/CronTests.swift
+++ Tests/JobsTests/Scheduler/CronTests.swift
@@ -5,44 +5,48 @@ import NIO
 final class CronTests: XCTestCase {
 
     func testInvalidCronStrings() throws {
+        var config = JobsConfig()
+
         // test not enough fields
-        XCTAssertThrowsError(try Scheduler().cron("* * * *"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("* * * *"))
 
         // test too many fields
-        XCTAssertThrowsError(try Scheduler().cron("*/5 * 4 * 1-3 1"))
-        XCTAssertThrowsError(try Scheduler().cron("*/5 * 4 * 1-3 abc"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("*/5 * 4 * 1-3 1"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("*/5 * 4 * 1-3 abc"))
 
         // test incorrect fields
-        XCTAssertThrowsError(try Scheduler().cron("-1 * * * 1"))
-        XCTAssertThrowsError(try Scheduler().cron("1-2-3 * * * 1"))
-        XCTAssertThrowsError(try Scheduler().cron("21* * * 1"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("-1 * * * 1"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("1-2-3 * * * 1"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("21* * * 1"))
 
         // test fields out of range
-        XCTAssertThrowsError(try Scheduler().cron("-1 * * * *"))
-        XCTAssertThrowsError(try Scheduler().cron("* -1 * * *"))
-        XCTAssertThrowsError(try Scheduler().cron("* * 0 * *"))
-        XCTAssertThrowsError(try Scheduler().cron("* * * 0 *"))
-        XCTAssertThrowsError(try Scheduler().cron("* * * * -1"))
-        XCTAssertThrowsError(try Scheduler().cron("60 * * * *"))
-        XCTAssertThrowsError(try Scheduler().cron("* 24 * * *"))
-        XCTAssertThrowsError(try Scheduler().cron("* * 32 * *"))
-        XCTAssertThrowsError(try Scheduler().cron("* * * 13 *"))
-        XCTAssertThrowsError(try Scheduler().cron("* * * * 7"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("-1 * * * *"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("* -1 * * *"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("* * 0 * *"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("* * * 0 *"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("* * * * -1"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("60 * * * *"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("* 24 * * *"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("* * 32 * *"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("* * * 13 *"))
+        XCTAssertThrowsError(try config.schedule(JobMock<JobDataMock>()).cron("* * * * 7"))
     }
 
     func testCronJobParser() throws {
+        var config = JobsConfig()
+
         // test good cron strings
-        XCTAssertNoThrow(try Scheduler().cron("*/5 * 4 * 1-3"))
-        XCTAssertNoThrow(try Scheduler().cron("1 2 3 4 5"))
-        XCTAssertNoThrow(try Scheduler().cron("*/1 */2 */3 */4 */5"))
-        XCTAssertNoThrow(try Scheduler().cron("1-5 1-5 1-5 1-5 1-5"))
-        XCTAssertNoThrow(try Scheduler().cron("1,3,5 1,3,5 1,3,5 1,3,5 1,3,5"))
+        XCTAssertNoThrow(try config.schedule(JobMock<JobDataMock>()).cron("*/5 * 4 * 1-3"))
+        XCTAssertNoThrow(try config.schedule(JobMock<JobDataMock>()).cron("1 2 3 4 5"))
+        XCTAssertNoThrow(try config.schedule(JobMock<JobDataMock>()).cron("*/1 */2 */3 */4 */5"))
+        XCTAssertNoThrow(try config.schedule(JobMock<JobDataMock>()).cron("1-5 1-5 1-5 1-5 1-5"))
+        XCTAssertNoThrow(try config.schedule(JobMock<JobDataMock>()).cron("1,3,5 1,3,5 1,3,5 1,3,5 1,3,5"))
 
         // test all stars
-        XCTAssertNoThrow(try Scheduler().cron("* * * * *"))
+        XCTAssertNoThrow(try config.schedule(JobMock<JobDataMock>()).cron("* * * * *"))
 
         // test spaces
-        XCTAssertNoThrow(try Scheduler().cron("*/5    *   4 * 1-3  "))
+        XCTAssertNoThrow(try config.schedule(JobMock<JobDataMock>()).cron("*/5    *   4 * 1-3  "))
     }
 
     static var allTests = [
