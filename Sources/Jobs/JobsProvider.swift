7c80c55b4e31f9cee6f0c1cf8582c3ac8eb1388d
Tanner Nelson <me@tanner.xyz>
2019-06-21 12:53:11 -0400

    provider updates

--- Sources/Jobs/JobsProvider.swift
+++ Sources/Jobs/JobsProvider.swift
@@ -4,17 +4,34 @@ import NIO
 
 /// A provider used to setup the `Jobs` package
 public struct JobsProvider: Provider {
-    
     /// See `Provider`.`register(_ services:)`
     public func register(_ s: inout Services) {
-        s.register(
-            QueueService.self
-        ) { container -> QueueService in
-            let persistenceLayer = try container.make(JobsPersistenceLayer.self)
-            
-            return QueueService(refreshInterval: self.refreshInterval,
-                                persistenceLayer: persistenceLayer,
-                                persistenceKey: self.persistenceKey)
+        s.register(QueueService.self) { container in
+            return try QueueService(
+                refreshInterval: self.refreshInterval,
+                persistenceLayer: container.make(JobsPersistenceLayer.self),
+                persistenceKey: self.persistenceKey
+            )
+        }
+
+        s.register(JobsConfiguration.self) { container in
+            return JobsConfiguration()
+        }
+
+        s.register(JobsCommand.self) { c in
+            return try .init(
+                queueService: c.make(),
+                jobContext: c.make(),
+                configuration: c.make()
+            )
+        }
+
+        s.register(JobContext.self) { c in
+            return .init(eventLoop: c.eventLoop)
+        }
+
+        s.extend(CommandConfiguration.self) { configuration, c in
+            try configuration.use(c.make(JobsCommand.self), as: "jobs")
         }
     }
     
