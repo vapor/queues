b7a30b375ed5e87364fcfa15ae3683e1e5043497
Tony Arnold <tony@thecocoabots.com>
2019-10-30 16:18:30 +1100

    Fix a logic inversion when checking for delayed jobs

--- Sources/Jobs/JobsCommand.swift
+++ Sources/Jobs/JobsCommand.swift
@@ -119,13 +119,12 @@ public class JobsCommand: Command {
                 // If the job has a delay, we must check to make sure we can execute
                 if let delay = jobStorage.delayUntil {
                     if delay >= Date() {
-                        console.info("Proceeding with delayed Job job_id=[\(jobStorage.id)]", newLine: true)
-                    } else {
                         // The delay has not passed yet, requeue the job
                         console.info("Requeing Job job_id=[\(jobStorage.id)] until \(delay)", newLine: true)
                         return queueService.persistenceLayer.requeue(key: key, jobStorage: jobStorage)
+                    } else {
+                        console.info("Proceeding with delayed Job job_id=[\(jobStorage.id)]", newLine: true)
                     }
-                    
                 }
 
                 guard let job = config.make(for: jobStorage.jobName) else {
