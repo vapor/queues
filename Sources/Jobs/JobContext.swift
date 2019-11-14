da4fd0215748eac677fffdfde980c44b2eadea57
Tony Arnold <tony@thecocoabots.com>
2019-11-15 09:34:58 +1100

    Change JobContext.userInfo dictionary to accept AnyHashable keys

--- Sources/Jobs/JobContext.swift
+++ Sources/Jobs/JobContext.swift
@@ -3,9 +3,8 @@ import Vapor
 
 /// A simple wrapper to hold job context and services.
 public struct JobContext: Service {
-    
     /// Storage for the wrapper.
-    public var userInfo: [String: Any]
+    public var userInfo: [AnyHashable: Any]
     
     /// Event loop for jobs to access
     public var eventLoop: EventLoop
