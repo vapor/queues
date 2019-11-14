22833b7a61ab15088355e213c000fc930a8f3569
Tony Arnold <tony@thecocoabots.com>
2019-11-15 09:52:14 +1100

    Update the API of JobsConfiguration to (mostly) match master
    I've left out the Logging module

--- Sources/Jobs/Job.swift
+++ Sources/Jobs/Job.swift
@@ -78,3 +78,23 @@ public protocol AnyJob {
     /// - Returns: A future void, signifying completion
     func error(_ context: JobContext, _ error: Error, _ storage: JobStorage) -> EventLoopFuture<Void>
 }
+
+// MARK: Scheduled
+
+/// Describes a job that can be scheduled and repeated
+public protocol ScheduledJob {
+
+    /// The method called when the job is run
+    /// - Parameter context: A `JobContext` that can be used
+    func run(context: JobContext) -> EventLoopFuture<Void>
+}
+
+class AnyScheduledJob {
+    let job: ScheduledJob
+    let scheduler: ScheduleBuilder
+
+    init(job: ScheduledJob, scheduler: ScheduleBuilder) {
+        self.job = job
+        self.scheduler = scheduler
+    }
+}
--- Sources/Jobs/JobsConfiguration.swift
+++ Sources/Jobs/JobsConfiguration.swift
@@ -3,29 +3,60 @@ import Vapor
 
 /// A `Service` to configure `Job`s
 public struct JobsConfiguration: Service {
-    
     /// Type storage
     internal var storage: [String: AnyJob]
-    
+
+    /// Scheduled Job Storage
+    internal var scheduledStorage: [AnyScheduledJob]
+
+    /// The number of seconds to wait before checking for the next job. Defaults to `1`
+    public var refreshInterval: TimeAmount
+
+    /// The key that stores the data about a job. Defaults to `vapor_jobs`
+    public var persistenceKey: String
+
+    public var userInfo: [AnyHashable: Any]
+
     /// Creates an empty `JobsConfig`
-    public init() {
-        storage = [:]
+    public init(refreshInterval: TimeAmount = .seconds(1), persistenceKey: String = "vapor_jobs") {
+        self.storage = [:]
+        self.scheduledStorage = []
+        self.refreshInterval = refreshInterval
+        self.persistenceKey = persistenceKey
+        self.userInfo = [:]
     }
-    
+
     /// Adds a new `Job` to the queue configuration.
     /// This must be called on all `Job` objects before they can be run in a queue.
     ///
     /// - Parameter job: The `Job` to add.
-    mutating public func add<J: Job>(_ job: J) {
-        let key = String(describing: J.Data.self)
+    mutating public func add<J>(_ job: J)
+        where J: Job
+    {
+        let key = J.jobName
         if let existing = storage[key] {
             print("WARNING: A job is already registered with key \(key): \(existing)")
         }
-        
-        storage[key] = job
+        self.storage[key] = job
     }
-    
-    
+
+    /// Schedules a new job for execution at a later date.
+    ///
+    ///     config.schedule(Cleanup())
+    ///     .yearly()
+    ///     .in(.may)
+    ///     .on(23)
+    ///     .at(.noon)
+    ///
+    /// - Parameter job: The `ScheduledJob` to be scheduled.
+    mutating internal func schedule<J>(_ job: J, builder: ScheduleBuilder = ScheduleBuilder()) -> ScheduleBuilder
+        where J: ScheduledJob
+    {
+        let storage = AnyScheduledJob(job: job, scheduler: builder)
+        self.scheduledStorage.append(storage)
+        return builder
+    }
+
     /// Returns the `AnyJob` for the string it was registered under
     ///
     /// - Parameter key: The key of the job
