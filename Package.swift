91621b09d03a69b1dee69ecabb5260ceaa56a5fa
Gwynne Raskind <gwynne@darkrainfall.org>
2020-02-29 13:57:24 -0600

    Update to match Vapor's OS requirement (#62)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "queues",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
     ],
     products: [
         .library(name: "Queues", targets: ["Queues"]),
