2e70a0c9e05b00c29a96b57442b4ea3abc61fd82
Ian Leitch <port001@gmail.com>
2020-05-20 18:01:11 +0200

    Update queue helper documentation.

--- Sources/Queues/QueueContext.swift
+++ Sources/Queues/QueueContext.swift
@@ -38,12 +38,12 @@ public struct QueueContext {
         self.eventLoop = eventLoop
     }
 
-    /// The `JobsQueue` object
+    /// Returns the default job `Queue`
     public var queue: Queue {
         self.queues(.default)
     }
 
-    /// A jobs queue for a specific queue name
+    /// Returns the specific job `Queue` for the given queue name
     /// - Parameter queue: The queue name
     public func queues(_ queue: QueueName) -> Queue {
         self.application.queues.queue(
