3dc80954c672effa188d01a15f56bf358c34beac
Gwynne Raskind <gwynne@vapor.codes>
2023-08-01 10:12:43 -0500

    Misc cleanups (#123)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5.2
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
@@ -14,8 +14,8 @@ let package = Package(
         .library(name: "XCTQueues", targets: ["XCTQueues"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.47.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.76.2"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.53.0"),
     ],
     targets: [
         .target(name: "Queues", dependencies: [
--- Sources/Queues/Exports.swift
+++ Sources/Queues/Exports.swift
@@ -1,4 +1,13 @@
-#if !BUILDING_DOCC
+#if swift(>=5.8)
+
+@_documentation(visibility: internal) @_exported import struct Foundation.Date
+@_documentation(visibility: internal) @_exported import struct Logging.Logger
+@_documentation(visibility: internal) @_exported import class NIO.EventLoopFuture
+@_documentation(visibility: internal) @_exported import struct NIO.EventLoopPromise
+@_documentation(visibility: internal) @_exported import protocol NIO.EventLoop
+@_documentation(visibility: internal) @_exported import struct NIO.TimeAmount
+
+#else
 
 @_exported import struct Foundation.Date
 @_exported import struct Logging.Logger
