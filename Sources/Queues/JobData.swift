63fa27a79731bba056e56adf0d5d11b412d832c2
Gwynne Raskind <gwynne@vapor.codes>
2024-06-01 01:44:58 -0500

    Add some utilities to JobData

--- Sources/Queues/JobData.swift
+++ Sources/Queues/JobData.swift
@@ -29,11 +29,45 @@ public struct JobData: Codable, Sendable {
         queuedAt: Date,
         attempts: Int = 0
     ) {
+        precondition(maxRetryCount >= 0)
+        precondition(attempts >= 0)
+        
         self.payload = payload
         self.maxRetryCount = maxRetryCount
         self.jobName = jobName
         self.delayUntil = delayUntil
         self.queuedAt = queuedAt
         self.attempts = attempts
+    }
+}
+
+// N.B.: These methods are intended for internal use only.
+extension JobData {
+    /// The non-`nil` number of attempts made to run this job (how many times has it failed).
+    /// This can also be treated as a "retry" count.
+    ///
+    /// Value | Meaning
+    /// -|-
+    /// 0 | The job has never run, or succeeded on its first attempt
+    /// 1 | The job has failed once and is queued for its first retry
+    /// 2... | The job has failed N times and is queued for its Nth retry
+    var failureCount: Int {
+        self.attempts ?? 0
+    }
+    
+    /// The number of retries left iff the current (re)try fails.
+    var remainingAttempts: Int {
+        Swift.max(0, self.maxRetryCount - self.failureCount)
+    }
+    
+    /// The current attempt number.
+    ///
+    /// Value|Meaning
+    /// -|-
+    /// 0|Not valid
+    /// 1|The job has not failed thus far; this the first attempt.
+    /// 2|The job has failed once; this is the second attempt.
+    var currentAttempt: Int {
+        self.failureCount + 1
     }
 }
