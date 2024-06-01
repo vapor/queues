32f15cc90beadfff52f6d0c05c286501f05fb102
Gwynne Raskind <gwynne@vapor.codes>
2024-06-01 01:45:11 -0500

    Package cleanup

--- Sources/Queues/Exports.swift
+++ Sources/Queues/Exports.swift
@@ -1,19 +1,6 @@
-#if swift(>=5.8)
-
 @_documentation(visibility: internal) @_exported import struct Foundation.Date
 @_documentation(visibility: internal) @_exported import struct Logging.Logger
 @_documentation(visibility: internal) @_exported import class NIOCore.EventLoopFuture
 @_documentation(visibility: internal) @_exported import struct NIOCore.EventLoopPromise
 @_documentation(visibility: internal) @_exported import protocol NIOCore.EventLoop
 @_documentation(visibility: internal) @_exported import struct NIOCore.TimeAmount
-
-#else
-
-@_exported import struct Foundation.Date
-@_exported import struct Logging.Logger
-@_exported import class NIOCore.EventLoopFuture
-@_exported import struct NIOCore.EventLoopPromise
-@_exported import protocol NIOCore.EventLoop
-@_exported import struct NIOCore.TimeAmount
-
-#endif
